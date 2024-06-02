40e96817c281c76870ce7a1d747c55f4b8d18bc9
mohammad1naqvi <104445058+mohammad1naqvi@users.noreply.github.com>
2024-06-02 10:55:30 +0000

    SunadaySecond

---
+++ .devcontainer/01_basic/operations.js
@@ -0,0 +1,18 @@
+let a = 20;
+let b = 30 ;
+let addition = a+b;
+let subtraction = a-b;
+let multiplication = a*b;
+let division = a/b;
+let remainder = b%a;
+let increment = ++a;
+let decrement = --b;
+console.log("value of a is :"+a);
+console.log("value of a b :"+b);
+console.log("Addition os the number is :"+addition);
+console.log("subtraction of the number is :"+subtraction);
+console.log("Multiplication of the number is :"+multiplication);
+console.log("Division of the number is :"+division);
+console.log("Division of the numbe ris :"+remainder);
+console.log("Increment os a is :"+increment);
+console.log("Decrement of b is :"+decrement)
