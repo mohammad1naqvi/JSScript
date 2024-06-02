50bc38aaa71392f9da0a33f50766b7888f93cb66
mohammad1naqvi <104445058+mohammad1naqvi@users.noreply.github.com>
2024-06-02 11:08:02 +0000

    SundayThird

---
+++ .devcontainer/01_basic/comparision.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 23;
+let aGreater = a>b;
+let bGreater = b>a;
+if(a==b){
+    console.log("The numbers are equal");
+}
+else{
+    console.log("Numbers are not equal");
+}
+console.log(aGreater);
+console.log(bGreater);
